52f3026804960911ceb85efb52b224fa74705206
James Radtke <ridexabide@gmail.com>
2019-10-30 10:43:10 -0500

    Moved

---
+++ Scripts/finish_RH8-UTIL-SRV01.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+PWD=`pwd`
+DATE=`date +%Y%m%d`
+ARCH=`uname -p`
+YUM=$(which yum)
+
+if [ `/bin/whoami` != "root" ]
+then
+  echo "ERROR:  You should be root to run this..."
+  exit 9
+fi
+
+# Repo/Channel Management
+# Typically you would not need to do this
+subscription-manager repos --disable="*" --enable=rhel-8-for-x86_64-baseos-rpms --enable=rhel-8-for-x86_64-supplementary-rpms --enable=rhel-8-for-x86_64-appstream-rpms
+# Install EPEL (needed for Fail2Ban)
+dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
+# Manage Security (local)
+## Disable Passphrase Logins (keys only)
+sed -i -e 's/^PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+systemctl restart sshd
+
+id -u jradtke &>/dev/null || useradd -u2025 -G10 -c "James Radtke" -p '$6$MIxbq9WNh2oCmaqT$10PxCiJVStBELFM.AKTV3RqRUmqGryrpIStH5wl6YNpAtaQw.Nc/lkk0FT9RdnKlEJEuB81af6GWoBnPFKqIh.' jradtke
+chage -l jradtke
+
+# Install/configure Fail2Ban
+dnf -y install fail2ban
+cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+
+cat << EOF > /etc/fail2ban/jail.local
+[DEFAULT]
+ignoreip = 192.168.0.0/24
+bantime  = 21600
+findtime  = 300
+maxretry = 3
+banaction = iptables-multiport
+backend = systemd
+
+[sshd]
+port    = ssh
+logpath = %(sshd_log)s
+backend = %(sshd_backend)s
+enabled = true
+EOF
+
+systemctl enable --now fail2ban
+
+
+# Install/configure AIDE
+
+
---
+++ Scripts/finish_apoc.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+PWD=`pwd`
+DATE=`date +%Y%m%d`
+ARCH=`uname -p`
+YUM=$(which yum)
+
+if [ `/bin/whoami` != "root" ]
+then
+  echo "ERROR:  You should be root to run this..."
+  exit 9
+fi
+
+# Manage NTP
+LINENUM=$(grep -n "#allow 192.168.0.0" /etc/chrony.conf | cut -f1 -d\:)
+sed -i -e "${LINENUM}iallow 10.10.10.0\/24" /etc/chrony.conf
+systemctl enable --now chronyd; systemctl start chronyd;
+#chronyd -q 'pool 0.rhel.pool.ntp.org iburst';
+chronyc -a 'burst 4/4'; sleep 10; chronyc -a makestep; sleep 2; hwclock --systohc; chronyc sources
+
+# Manage Filesystems/Directories and Storage
+mkdir /data
+cp /etc/fstab /etc/fstab.orig
+echo "# NON-Root Mounts" >> /etc/fstab
+echo "/dev/mapper/vg_data-lv_data /data xfs defaults 0 0" >> /etc/fstab
+mount -a || exit 9
+
+echo "# BIND mounts" >> /etc/fstab
+echo "/data/images /var/lib/libvirt/images none bind,defaults 0 0" >> /etc/fstab
+mount -a
+
+# Manage Packages
+PACKAGES="git"
+yum -y install $PACKAGES
+
+#####################################
+#####################################
+#
+#     Setup Virtualization
+#
+#####################################
+#####################################
+yum -y groupinstall "Virtualization Host"
+yum -y install virt-* 
+# Configure Network Bridge
+INTERFACE=eno1
+cat << EOF > /root/nmcli_cmds.sh
+nmcli con add type bridge autoconnect yes con-name brkvm ifname brkvm ip4 10.10.10.18/24 gw4 10.10.10.1
+nmcli con modify brkvm ipv4.address 10.10.10.18/24 ipv4.method manual
+nmcli con modify brkvm ipv4.gateway 10.10.10.1
+nmcli con modify brkvm ipv4.dns "10.10.10.121"
+nmcli con modify brkvm +ipv4.dns "10.10.10.122"
+nmcli con modify brkvm +ipv4.dns "8.8.8.8"
+nmcli con modify brkvm ipv4.dns-search "matrix.lab"
+nmcli con delete $INTERFACE
+nmcli con add type bridge-slave autoconnect yes con-name $INTERFACE ifname $INTERFACE master brkvm
+systemctl stop NetworkManager; systemctl start NetworkManager
+EOF
+sh /root/nmcli_cmds.sh &
+
