cc51b4ea8eaa6effa444cf5982131a2127de9183
James Radtke <ridexabide@gmail.com>
2019-10-30 19:34:10 -0500

    Clean up

--- Scripts/docker_setup.sh
+++ Scripts/docker_setup.sh
@@ -5,5 +5,8 @@ yum -y install docker
 cat << EOF > /etc/sysconfig/docker-storage-setup
 VG=docker-vg
 DEVS=/dev/vdb
+CONTAINER_ROOT_LV_NAME="docker-root-lv"
+CONTAINER_ROOT_LV_SIZE="100%FREE"
+CONTAINER_ROOT_LV_MOUNT_PATH="/var/lib/docker"
 EOF
 docker-storage-setup
