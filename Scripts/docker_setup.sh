7d655b91546345e14c0a84547ea68475f12a2a16
James Radtke <cloudxabide@gmail.com>
2019-12-03 15:38:04 -0500

    Updates

--- Scripts/docker_setup.sh
+++ Scripts/docker_setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# If docker-vg exists, then exit
+(vgs docker-vg) && { echo "Docker already installed"; exit 9; }
+
 yum -y install docker
 
 cat << EOF > /etc/sysconfig/docker-storage-setup
@@ -11,6 +14,5 @@ CONTAINER_ROOT_LV_SIZE="100%FREE"
 CONTAINER_ROOT_LV_MOUNT_PATH="/var/lib/docker"
 EOF
 
-# If there is not a docker-vg present, then create it
-vgs docker-vg || docker-storage-setup
+docker-storage-setup
 systemctl enable docker --now
