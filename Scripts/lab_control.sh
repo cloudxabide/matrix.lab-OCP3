3e280cef6e4b060007b02ef55f7b2b4cb5bafb23
James Radtke <cloudxabide@gmail.com>
2019-12-07 09:30:09 -0600

    New File

---
+++ Scripts/lab_control.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# TODO:  I'll make this accept parameters - for now it's just to build
+
+[ ! -d ~/matrix.lab ] && { cd; git clone https://github.com/cloudxabide/matrix.lab; cd ~/matrix.lab/Scripts/ } || { cd ~/matrix.lab/Scripts/; git pull; }
+
+
+SLEEPYTIME=200;
+HYPERVISOR=`hostname -s`
+for GUEST in `grep -v \# .myconfig | grep  $HYPERVISOR | awk -F: '{ print $1 }'`
+do
+  echo "./build_KVM.sh $GUEST"
+  COUNTER=${SLEEPYTIME}; ./build_KVM.sh $GUEST; while [ $COUNTER -gt 0 ]; do echo -ne "Proceed in: $COUNTER\033[0K\r"; sleep 1; : $((COUNTER--)); done;
+done
+
