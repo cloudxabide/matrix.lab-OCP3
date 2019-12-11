c6034ff5c137df808f0db02fc19ce74818198149
James Radtke <cloudxabide@gmail.com>
2019-12-11 15:23:30 -0500

    Adding features

--- Scripts/lab_control.sh
+++ Scripts/lab_control.sh
@@ -2,6 +2,13 @@
 
 # TODO:  I'll make this accept parameters - for now it's just to build
 
+usage() {
+  echo "ERROR:"
+  echo "usage:  ${0} <build|stop|start|update>"
+  echo ""
+  exit 9 
+}
+ 
 example() {
 HYPERVISORS="apoc neo trinty morpheus"
 for HYPERVISOR in $HYPERVISORS
@@ -13,6 +20,7 @@ EOF
 done
 }
 
+##################################### ##########################################
 # Make sure lab is up-to-date
 get_to_gittin() {
 HYPERVISORS="apoc neo trinity morpheus zion sati"
@@ -25,18 +33,25 @@ EOF
 done
 }
 
+##################################### ##########################################
+update() {
 [ ! -d ~/matrix.lab ] && { cd; git clone https://github.com/cloudxabide/matrix.lab; cd ~/matrix.lab/Scripts/; } || { cd ~/matrix.lab/Scripts/; git pull; }
+}
 
+##################################### ##########################################
+build_VMS() {
 SLEEPYTIME=200;
 HYPERVISOR=`hostname -s`
+echo "Deploying VMs"
 for GUEST in `grep -v \# .myconfig | grep  $HYPERVISOR | awk -F: '{ print $1 }'`
 do
   echo "./build_KVM.sh $GUEST"
-  COUNTER=${SLEEPYTIME}; ./build_KVM.sh $GUEST; while [ $COUNTER -gt 0 ]; do echo -ne "Proceed in: $COUNTER\033[0K\r"; sleep 1; : $((COUNTER--)); done;
+  #COUNTER=${SLEEPYTIME}; ./build_KVM.sh $GUEST; while [ $COUNTER -gt 0 ]; do echo -ne "Proceed in: $COUNTER\033[0K\r"; sleep 1; : $((COUNTER--)); done;
 done
+}
 
+##################################### ##########################################
 teardown() {
-
 for HOST in `virsh list --all | grep OCP | awk '{ print $2 }'`; do virsh snapshot-delete $HOST post-install-snap; done
 for HOST in `virsh list --all | grep OCP | awk '{ print $2 }'`; do virsh destroy $HOST; done
 for HOST in `virsh list --all | grep OCP | awk '{ print $2 }'`; do rm -f /var/lib/libvirt/images/$HOST/*; done
@@ -48,7 +63,20 @@ systemctl restart libvirtd
 
 }
 
+##################################### ##########################################
 startup() {
 for HOST in `virsh list --all | grep OCP | awk '{ print $2 }'`; do virsh start $HOST; done
 }
+
+
+if [ $# -ne 1 ]; then usage; fi
+
+case $1 in 
+  start) start ;;
+  stop) stop ;;
+  build) build_VMS ;;
+  update) update ;;
+  *) usage ;;
+esac
+
 exit 0
