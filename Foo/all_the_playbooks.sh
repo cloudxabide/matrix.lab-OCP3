aac58f9bf18965bbc6a68b15b3827937df6d6f69
James Radtke <cloudxabide@gmail.com>
2020-01-02 21:00:12 -0500

    Update Version

--- Foo/all_the_playbooks.sh
+++ Foo/all_the_playbooks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # for FILE in `grep failed=1 * | awk -F: '{ print $1 }' | sort -u`; do echo "$FILE"; grep "plays in" $FILE; echo;  done
-BASE="${HOME}/ocp-3.11-1212"
+BASE="${HOME}/ocp-3.11-1112"
 INVENTORY="${BASE}.yml"
 LOGDATE=`date +%Y%m%d`; LOGDIR="${HOME}/${LOGDATE}/"; mkdir -p $LOGDIR; cd $LOGDIR
 PLAYBOOKS="/usr/share/ansible/openshift-ansible/playbooks/"
