7d17d0562d1eb7e2b47bb79b70482965cd4856a8
James Radtke <cloudxabide@gmail.com>
2020-01-23 12:47:51 -0500

    Resizing storage

--- Scripts/deploy_example_apps_OCP3.sh
+++ Scripts/deploy_example_apps_OCP3.sh
@@ -11,11 +11,13 @@ oc login --certificate-authority=rh7-ocp3-mst.matrix.lab.pem --username=morpheus
 ## Create Your Project and Deploy App
 ```
 # HexGL is a HTML5 video game resembling WipeOut from back in the day (Hack the Planet!)
-MYPROJ="hexgl"
+MYPROJ="hexgl1"
 oc new-project $MYPROJ
 oc new-app php:5.6~https://github.com/cloudxabide/HexGL.git
-# Create a secure route
-echo '{ "kind": "List", "apiVersion": "v1", "metadata": {}, "items": [ { "kind": "Route", "apiVersion": "v1", "metadata": { "name": "hexgl", "creationTimestamp": null, "labels": { "app": "hexgl" } }, "spec": { "host": "hexgl.ocp3-mwn.linuxrevolution.com", "to": { "kind": "Service", "name": "hexgl" }, "port": { "targetPort": 8080 }, "tls": { "termination": "edge" } }, "status": {} } ] }' | oc create -f -
+# Create a secure route (hexgl.ocp3-mwn.linuxrevolution.com)
+#echo '{ "kind": "List", "apiVersion": "v1", "metadata": {}, "items": [ { "kind": "Route", "apiVersion": "v1", "metadata": { "name": "hexgl", "creationTimestamp": null, "labels": { "app": "hexgl" } }, "spec": { "host": "hexgl.ocp3-mwn.linuxrevolution.com", "to": { "kind": "Service", "name": "hexgl" }, "port": { "targetPort": 8080 }, "tls": { "termination": "edge" } }, "status": {} } ] }' | oc create -f -
+# Or this route (hexgl.linuxrevolution.com)
+echo '{ "kind": "List", "apiVersion": "v1", "metadata": {}, "items": [ { "kind": "Route", "apiVersion": "v1", "metadata": { "name": "hexgl", "creationTimestamp": null, "labels": { "app": "hexgl" } }, "spec": { "host": "hexgl.linuxrevolution.com", "to": { "kind": "Service", "name": "hexgl" }, "port": { "targetPort": 8080 }, "tls": { "termination": "edge" } }, "status": {} } ] }' | oc create -f -
 ```
 
 At some point you will be able to browse to:  
