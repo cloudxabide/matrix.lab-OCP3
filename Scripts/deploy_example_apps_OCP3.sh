1e4e7e273c8c384c34a7bc7ffd99efbc62d572e6
James Radtke <cloudxabide@gmail.com>
2020-03-12 22:16:13 -0400

    Cleanup

--- Scripts/deploy_example_apps_OCP3.sh
+++ Scripts/deploy_example_apps_OCP3.sh
@@ -20,7 +20,7 @@ oc new-app php:5.6~https://github.com/cloudxabide/HexGL.git
 echo '{ "kind": "List", "apiVersion": "v1", "metadata": {}, "items": [ { "kind": "Route", "apiVersion": "v1", "metadata": { "name": "hexgl", "creationTimestamp": null, "labels": { "app": "hexgl" } }, "spec": { "host": "hexgl.linuxrevolution.com", "to": { "kind": "Service", "name": "hexgl" }, "port": { "targetPort": 8080 }, "tls": { "termination": "edge" } }, "status": {} } ] }' | oc create -f -
 ```
 
-At some point you will be able to browse to:  
+At some point you will be able to browse to (depending on the route you enabled):  
 https://hexgl.ocp3-mwn.linuxrevolution.com/
 https://hexgl.linuxrevolution.com/
 
