8c8e41431b77f1ae130a9fd0944f4762cf47c8f9
James Radtke <emailaddy@gmail.com>
2020-05-05 11:59:14 -0500

    Unnecessary

--- Scripts/finish_RH7-OCP3-BST01.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#  NOTES:  Separated this to its own script
-# STATUS:  Not even close to finished and not that important right now.
-#   TODO:  add post_install.sh, docker stuff, channel mgmt
-
-# Install supporting pakcages on Bastion
-yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
-
